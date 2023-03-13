ed5156417ff4897e6de70364cb00501743893419
Rinaldo23 <rinaldobadigar@gmail.com>
2023-03-13 22:44:41 +0530

    Added Scroll to Top Button

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import ContactImg from '../../public/assets/Contact.jpg';
 
 const Contact = () => {
@@ -120,7 +121,20 @@ const Contact = () => {
                             </form>
                         </div>
                     </div>
+
                 </div>
+
+                <div className='flex justify-center py-12'>
+                    <Link href='/'>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <HiOutlineChevronDoubleUp
+                                className='text-[#5651e5]'
+                                size={30}
+                            />
+                        </div>
+                    </Link>
+                </div>
+                
             </div>
         </div>
     );
