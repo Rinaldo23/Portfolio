dd50d44489b3b5723ffec7d27859219574bcd374
Rinaldo23 <rinaldobadigar@gmail.com>
2023-03-13 22:41:34 +0530

    Contact Section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from 'next/head'
+import About from './components/About'
+import Contact from './components/Contact'
+import Main from './components/Main'
 import Navbar from './components/NavBar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 export default function Home() {
   return (
@@ -11,6 +16,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact/>
     </>
   )
 }
