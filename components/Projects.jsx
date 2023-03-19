7bba6cb888c4719c32d5ceceddd2f5393abc75fb
Rinaldo23 <rinaldobadigar@gmail.com>
2023-03-19 21:32:53 +0530

    Updated

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -17,6 +17,12 @@ const Projects = () => {
             tech: 'React'
         },
         {
+            title: 'AirBnb Clone',
+            backgroundImg: "/../public/assets/projects/AirBnbClone.png",
+            projectUrl: '/airbnbclone',
+            tech: 'MERN'
+        },
+        {
             title: 'Todo App',
             backgroundImg: "/../public/assets/projects/TodoApp.png",
             projectUrl: '/todo',
@@ -28,12 +34,6 @@ const Projects = () => {
             projectUrl: '/filemanager',
             tech: 'Node'
         },
-        {
-            title: 'AirBnb Clone',
-            backgroundImg: "/../public/assets/projects/AirBnbClone.png",
-            projectUrl: '/airbnbclone',
-            tech: 'MERN'
-        }
     ]
 
     const [items, setItems] = useState(projects);
@@ -54,16 +54,16 @@ const Projects = () => {
                 <p className='text-xl tracking-widest uppercase text-[#5651e5] dark:text-white'>Projects</p>
                 <h2 className='py-4 dark:text-[#a6b595]'>What I&apos;ve Built</h2>
 
-                <nav className="flex justify-center space-x-6 px-1 border border-black max-w-fit m-auto mb-4 rounded-3xl dark:bg-gray-500">
+                {/* <nav className="flex justify-center space-x-6 px-1 border border-black max-w-fit m-auto mb-4 rounded-3xl dark:bg-gray-500">
                     <a onClick={() => filterItem("ALL")} className="font-me px-2 py-2 cursor-pointer text-slate-700 rounded-2xl hover:bg-gray-300 hover:text-slate-900">ALL</a>
                     <a onClick={() => filterItem('Node')} className="font-me px-2 py-2 cursor-pointer text-slate-700 rounded-2xl hover:bg-gray-300 hover:text-slate-900">NODE</a>
                     <a onClick={() => filterItem("DOM")} className="font-me px-2 py-2 cursor-pointer text-slate-700 rounded-2xl hover:bg-gray-300 hover:text-slate-900">DOM</a>
                     <a onClick={() => filterItem("React")} className="font-me px-2 py-2 cursor-pointer text-slate-700 rounded-2xl hover:bg-gray-300 hover:text-slate-900">REACT</a>
                     <a onClick={() => filterItem("MERN")} className="font-me px-2 py-2 cursor-pointer text-slate-700 rounded-2xl hover:bg-gray-300 hover:text-slate-900">MERN</a>
-                </nav>
+                </nav> */}
                 <div className='grid md:grid-cols-3 gap-8'>
 
-                    {
+                    {/* {
                         items.map((item, idx) => (
                             // console.log(item)
                             <ProjectItem
@@ -75,43 +75,44 @@ const Projects = () => {
                             />
 
                         ))
-                    }
-                    {
-                        <ProjectItem
-                        title='YouTubeClone'
+                    } */}
+
+                    <ProjectItem
+                        title={projects[0].title}
                         backgroundImg={projects[0].backgroundImg}
-                        projectUrl='/youtubeclone'
-                        tech='MERN'
-                    />
-                    }
-
-                    {/* <ProjectItem
-                        title='YouTube Clone'
-                        backgroundImg={YouTubeClone}
-                        projectUrl='/youtubeclone'
-                        tech='MERN'
-                    />
-                    <ProjectItem
-                        title='IMDb Clone'
-                        backgroundImg={IMDbClone}
-                        projectUrl='/imdbclone'
-                        tech='React JS'
+                        projectUrl={projects[0].projectUrl}
+                        tech={projects[0].tech}
 
                     />
                     <ProjectItem
-                        title='Netflix App'
-                        backgroundImg={IMDbClone}
-                        projectUrl='/'
-                        tech='React JS'
+                        title={projects[1].title}
+                        backgroundImg={projects[1].backgroundImg}
+                        projectUrl={projects[1].projectUrl}
+                        tech={projects[1].tech}
 
                     />
                     <ProjectItem
-                        title='Twitch UI'
-                        backgroundImg={IMDbClone}
-                        projectUrl='/'
-                        tech='Next JS'
+                        title={projects[2].title}
+                        backgroundImg={projects[2].backgroundImg}
+                        projectUrl={projects[2].projectUrl}
+                        tech={projects[2].tech}
 
-                    /> */}
+                    />
+                    <ProjectItem
+                        title={projects[3].title}
+                        backgroundImg={projects[3].backgroundImg}
+                        projectUrl={projects[3].projectUrl}
+                        tech={projects[3].tech}
+
+                    />
+                    <ProjectItem
+                        title={projects[4].title}
+                        backgroundImg={projects[4].backgroundImg}
+                        projectUrl={projects[4].projectUrl}
+                        tech={projects[4].tech}
+
+                    />
+
                 </div>
             </div>
         </div>
